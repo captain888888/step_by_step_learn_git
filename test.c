acb3a43444a3458c5a90f34bb04d8475b255f610
daikai <tornado_dai@163.com>
2016-05-02 08:51:22 -0700

    add for test github from ubuntu14.04

--- test.c
+++ test.c
@@ -2,4 +2,7 @@
 void main()
 {
   printf("This is test for github.\n");
+  /*add by daikai from ubuntn 14.04 PN:2016/05/02*/
+  printf("[Debug_daikai]This is add for github from ubuntu.\n");
+  /*add by daikai from ubuntn 14.04 PN:2016/05/02*/
 }
